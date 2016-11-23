ea745cbd78307453f413ef54b272fa58df704642
BloodRose86 <andre@aj-systems.co.za>
2016-11-23 21:50:25 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>{
+    console.log('New user connected');
+});
+
+io.of('connection', (socket) =>{
+    console.log('Disconnected from Server');
+});
+
 server.listen(port, () =>{
     console.log(`Server is up on port ${port}`);
 });
