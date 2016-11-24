6234987e1e237f1bdfbc23e56dea7eae49444db8
BloodRose86 <andre@aj-systems.co.za>
2016-11-24 19:14:15 +0200

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
 console.log('Connected to server');
 
-socket.emit ('createMessage', {
-        from: 'Andre',
-        text: 'Hey. This is MEEEE',
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -11,27 +11,22 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) =>{
+io.on('connection', (socket) => {
     console.log('New user connected');
-
-    socket.emit('newMessage', {
-        from: 'Lasy Lusie',
-        text: 'Hey. What is going on. Lasy',
-        createdAt: 123123
-    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
         console.log('User was Disconnected from Server');
         });
 });
-
-// io.of('connection', (socket) =>{
-//     console.log('Disconnected from Server');
-// });
 
 server.listen(port, () =>{
     console.log(`Server is up on port ${port}`);
