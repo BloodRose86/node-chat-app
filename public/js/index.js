a63bb0be0781e20fc1b0facd488a21ab3de4aa39
BloodRose86 <andre@aj-systems.co.za>
2016-12-10 10:22:26 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Andre',
-    text: 'Yup that went through'
-  });
 });
 
 socket.on('disconnect', function () {
